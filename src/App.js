de6179016b1fcc4ef1077f94270c4656f6fef0af
Yakau Trafimovich <5243212@gmail.com>
2022-05-11 14:31:06 +0200

    start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+    return (
+        <div>
+            <h1>Counter</h1>
+            <button className="btn btn-success">Add</button>
+            <button class="btn btn-danger">Delete</button>
+        </div>
+    )
+}
+
+export default App;
