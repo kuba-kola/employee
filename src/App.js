3816c6dc0b8de505a629eeecbe3d03243d535f08
Yakau Trafimovich <5243212@gmail.com>
2022-05-11 14:50:41 +0200

    add increment and decrement

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+    //first variable - this is the state (counter), second var - function that helps change counter
+    const [counter, setCounter] = useState(0)
+
+    function increment() {
+        //call function that change state and say what we want
+        setCounter(counter + 1)
+    }
+
+    function decrement() {
+        setCounter(counter - 1)
+    }
+
     return (
         <div>
-            <h1>Counter</h1>
-            <button className="btn btn-success">Add</button>
-            <button class="btn btn-danger">Delete</button>
+            <h1>Counter: {counter}</h1>
+            <button onClick={increment} className="btn btn-success">Add</button>
+            <button onClick={decrement} class="btn btn-danger">Delete</button>
         </div>
     )
 }
